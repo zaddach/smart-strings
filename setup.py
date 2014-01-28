b9f8287d3f774ea9e1a67b092b6776dd94139ef6
zaddach <zaddach@eurecom.fr>
2014-01-28 17:35:15 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(
+    name='smart-strings',
+    version='0.1.0dev1',
+    author='Jonas Zaddach',
+    author_email='zaddach@eurecom.fr',
+    packages=[],
+    url='https://github.com/zaddach/smart-strings',
+    license='MIT',
+    description='A smarter version of the unix strings utility',
+    long_description=open('README.md').read()
+)
