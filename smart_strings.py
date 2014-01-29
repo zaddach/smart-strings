f11ea251c59384320369cf0a1c469757bd054ca0
Jonas Zaddach <zaddach@eurecom.fr>
2014-01-29 14:36:49 +0100

    Added command line switches, fixed UTF-16, UTF-32 reading - Added command line switch '-t' to be compatible with traditional strings - Added --readable-percent switch that takes over the function of '--readable' to specify a percentage of expected readable characters - Removed '-m', '--max' switch to specify maximum string length - Changed '--readable' switch to not take an argument and require 80% of readable characters - Changed error handling of input decoder to 'ignore' so that invalid unicode sequences will not result in an error
    Signed-off-by: Jonas Zaddach <zaddach@eurecom.fr>

--- smart_strings.py
+++ smart_strings.py
@@ -53,14 +53,20 @@ def is_readable_string(value, percentage):
 def main(argv):
     parser = argparse.ArgumentParser()
     
-    parser.add_argument("-r", "--readable", nargs = '?', dest = "readable_percentage", const = 0.8, default = 0.0, type = float, choices = [FloatRange(0.0, 1.0)], help = "Part of the string that needs to consist of letters or digits")
-    parser.add_argument("-o", "--offset", action = "store_true", dest = "print_offset", help = "Print offset where string occurs")
-    parser.add_argument("-c", "--charset", type = str, default = "iso-8859-1", dest = "charset", help = "Input character set used for input stream in python (iso-8859-1, UTF-8, UTF-16, ...)")
-    parser.add_argument("-n", "--min", type = int, default = 5, dest = "min", help = "Minimum number of characters in the string, or in case of dictionary checking the minimum length of words")
-    parser.add_argument("-m", "--max", type = int, default = -1, dest = "max", help = "Maximum number of characters in the string")
+    offset_group = parser.add_mutually_exclusive_group()
+    offset_group.add_argument("-o", action = "store_const", const = "d", dest = "offset_format", help = "Print decimal offset where string occurs")
+    offset_group.add_argument("-t", "--offset", dest = "offset_format", choices = ["o", "d", "x"], help = "Print (o)ctal/(d)ecimal/he(x)adecimal offset where string occurs")
+
+    readable_group = parser.add_mutually_exclusive_group()
+    readable_group.add_argument("-r", "--readable", dest = "readable_percentage",  action = "store_const", default = 0.0, const = 0.8, help = "At least 80%% letters and digits in the string")
+    readable_group.add_argument("--readable-percentage", dest = "readable_percentage", type = float, help = "Part of the string in percent that needs to consist of letters or digits [0.0, 1.0]")
+    charset_group = parser.add_mutually_exclusive_group()
+    charset_group.add_argument("-c", "--charset", type = str, default = "iso-8859-1", dest = "charset", help = "Input character set used for input stream in python (iso-8859-1, UTF-8, UTF-16, ...)")
+#    charset_group.add_argument("-e", "--encoding", type = str, 
+    parser.add_argument("-n", "--bytes", type = int, default = 5, dest = "min", help = "Minimum number of characters in the string, or in case of dictionary checking the minimum length of words")
     parser.add_argument("-f", "--filter-nonprintables", action = "store_true", dest = "filter_nonprintables", help = "Replace nonprintable characters with spaces")
     if ENCHANT_AVAILABLE:
-        parser.add_argument("-d", "--dict", dest = "dicts", action = "append", help = "Use this dictionary to test if words exist (can be specified multiple times to use several dictionaries; use '?' to get a list of installed dictionaries)")
+        parser.add_argument("-d", "--dict", dest = "dicts", default = [], action = "append", help = "Use this dictionary to test if words exist (can be specified multiple times to use several dictionaries; use '?' to get a list of installed dictionaries)")
     parser.add_argument("input_file", type = str, metavar = "FILE", nargs = '?', const = None, default = None, help = "File to read from; do not specify to read from stdin")
 
     args = parser.parse_args(argv[1:])
@@ -75,16 +81,16 @@ def main(argv):
         dicts = []
     
     if not args.input_file is None:
-        input_stream = open(args.input_file, 'r', encoding = args.charset)
+        raw_stream = open(args.input_file, 'rb')
     else:
-        input_stream = codecs.getwriter(args.charset)(sys.stdin.buffer)
+        raw_stream = sys.stdin.buffer
+        
+    input_stream = codecs.getreader(args.charset)(raw_stream, errors = 'ignore')
         
     for (offset, value) in find_printables(input_stream):
         if not is_readable_string(value, args.readable_percentage):
             continue
         if len(value) < args.min:
-            continue
-        if args.max != -1 and len(value) > args.max:
             continue
         
         #Try to isolate words in the string and see if they are present in the dictionaries
@@ -107,11 +113,12 @@ def main(argv):
             value = "".join([x in PRINTABLE_CHARACTERS and x or " " for x in value.strip()])
         else:
             value = value.strip()
-        
-        if args.print_offset:
-            print("% 5d: %s" % (offset, value))
+       
+        if not args.offset_format is None:
+            offset_prefix = ("% 5" + args.offset_format + ": ") % (offset, )
         else:
-            print("%s" % (value, ))
+            offset_prefix = ""
+        print(offset_prefix + ("%s" % (value, )))
             
     input_stream.close()
 
